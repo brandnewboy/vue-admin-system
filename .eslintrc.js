3e458431fba2e0c4554d6fcaa11f4d1e862b319e
lizhang <2475934003@qq.com>
2022-07-15 21:46:38 +0800

    feat(.prettierrc.js .eslintrc.js .cz-config.js): 初始化项目,第一次提交,this is first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
+const prettierRules = require("./.prettierrc.js")
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "plugin:prettier/recommended"],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@babel/eslint-parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    "prettier/prettier": ["error", prettierRules]
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    semi: false,
+    singleQute: true,
+    trailingComma: "none"
+}
